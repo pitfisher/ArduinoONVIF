6f161bb14a28508127723b623d5d4e59f6941d19
Petr Rybakov <mypostbox01@mail.ru>
2022-02-14 03:52:12 +0300

    Rewrote ContinousMove using sprintf and ArduinoHttpClient Also minor updates in other functions

--- onvif.cpp
+++ onvif.cpp
@@ -17,6 +17,7 @@ char *httpHeaderPtzService = "POST /onvif/ptz_service ";
 char *httpHeaderStatic = "HTTP/1.1\r\nUser-Agent: Arduino/1.0\r\nAccept: */*\r\nContent-Type: application/soap+xml; charset=utf-8;\r\nContent-Length: ";
 char *soap_EnvelopeOpen = "<s:Envelope xmlns:s=\"http://www.w3.org/2003/05/soap-envelope\">";
 char *soap_EnvelopeClose = "</s:Envelope>";
+const char* contentType = "application/soap+xml; charset=utf-8";
 
 struct soap_Header soap_Header =  {
     .soap_HeaderOpen = "<s:Header>",
@@ -108,14 +109,13 @@ char *calculateHeaderSecurity(char *username, char *password, char *createTime, 
 void onvifGetHostname(IPAddress server){
   char* contentType = "application/soap+xml; charset=utf-8";
   char* postData = "<s:Envelope><s:Body><ns0:GetHostname xmlns:ns0=\"http://www.onvif.org/ver10/device/wsdl\"/></s:Body></s:Envelope>";
-  char serverAddress[] = "192.168.1.70";  // server address
   int port = 80;
-   // Initialize the Ethernet client library
+  // Initialize the Ethernet client library
   // with the IP address and port of the server
   // that you want to connect to (port 80 is default for HTTP):
   EthernetClient eth;
-  HttpClient client = HttpClient(eth, serverAddress, port);
-  client.post("http://192.168.1.70/onvif/device_service", contentType, postData);
+  HttpClient client = HttpClient(eth, server, port);
+  client.post("/onvif/device_service", contentType, postData);
   // read the status code and body of the response
   int statusCode = client.responseStatusCode();
   String response = client.responseBody();
@@ -125,17 +125,15 @@ void onvifGetHostname(IPAddress server){
   Serial.println(response);
 }
 
-void onvifGetSystemDateAndTime(IPAddress server, struct soap_Header *soap_Header, struct soap_Body *soap_Body) {
-  char* contentType = "application/soap+xml; charset=utf-8";
-  char* postData = "<s:Envelope><s:Body><ns0:GetSystemDateAndTime xmlns:ns0=\"http://www.onvif.org/ver10/device/wsdl\"/></s:Body></s:Envelope>";
-  char serverAddress[] = "192.168.1.70";  // server address
+void onvifGetSystemDateAndTime(IPAddress server) {
+  const char* postData = "<s:Envelope><s:Body><ns0:GetSystemDateAndTime xmlns:ns0=\"http://www.onvif.org/ver10/device/wsdl\"/></s:Body></s:Envelope>";
   int port = 80;
-   // Initialize the Ethernet client library
+  // Initialize the Ethernet client library
   // with the IP address and port of the server
   // that you want to connect to (port 80 is default for HTTP):
   EthernetClient eth;
-  HttpClient client = HttpClient(eth, serverAddress, port);
-  client.post("http://192.168.1.70/onvif/device_service", contentType, postData);
+  HttpClient client = HttpClient(eth, server, port);
+  client.post("/onvif/device_service", contentType, postData);
   // read the status code and body of the response
   int statusCode = client.responseStatusCode();
   String response = client.responseBody();
@@ -146,64 +144,26 @@ void onvifGetSystemDateAndTime(IPAddress server, struct soap_Header *soap_Header
 }
 
 void onvifContinuousMove(IPAddress server, struct soap_Header *soap_Header, struct soap_Body *soap_Body, char *ProfileToken, char* velocity_x, char* velocity_y) {
-  Serial.println(millis());	
-  EthernetClient client;
-//   soap_Header.soap_HeaderSecurity = (char*)malloc(2); //not sure how it works again; attempt to allocate proper amount of memory makes arduino hang
-//   strcpy(soap_Header.soap_HeaderSecurity, calculateHeaderSecurity(username, password, createTime, nonce));
-
-  if (client.connect(server, 80)) {
-    // Make a HTTP request:
-    client.print(httpHeaderPtzService);
-    client.print(httpHeaderStatic);
-//    client.println(10 + getSoapHeaderLength(soap_Header) + 8 + strlen("<ContinuousMove xmlns=\"http://www.onvif.org/ver20/ptz/wsdl\"><ProfileToken></ProfileToken><Velocity><PanTilt x=\"\" y=\"\" xmlns=\"http://www.onvif.org/ver10/schema\"/></Velocity></ContinuousMove>") + strlen(ProfileToken) + strlen(velocity_x) + strlen(velocity_y) + 9 + 11); //calculate and send Content-Length of request
-    Serial.println(strlen(soap_EnvelopeOpen) + getSoapHeaderLength(soap_Header) + strlen(soap_Body->soap_BodyOpen) + strlen("<ContinuousMove xmlns=\"http://www.onvif.org/ver20/ptz/wsdl\"><ProfileToken></ProfileToken><Velocity><PanTilt x=\"\" y=\"\" xmlns=\"http://www.onvif.org/ver10/schema\"/></Velocity></ContinuousMove>") + strlen(ProfileToken) + strlen(velocity_x) + strlen(velocity_y) + 9 + 11);
-    Serial.println(strlen(soap_EnvelopeOpen) + strlen("<s:Header><Security s:mustUnderstand=\"1\" xmlns=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd\"><UsernameToken><Username>admin</Username><Password Type=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordDigest\">K2IcvAJ0Mq/4Rv9jYyrKBsgiJ1c=</Password><Nonce EncodingType=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary\">TEbs4ad+2UWRiWKbPRxRyboEAAAAAA==</Nonce><Created xmlns=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd\">2022-02-11T22:32:40.593Z</Created></UsernameToken></Security></s:Header>") + strlen(soap_Body->soap_BodyOpen) + strlen("<ContinuousMove xmlns=\"http://www.onvif.org/ver20/ptz/wsdl\"><ProfileToken></ProfileToken><Velocity><PanTilt x=\"\" y=\"\" xmlns=\"http://www.onvif.org/ver10/schema\"/></Velocity></ContinuousMove>") + strlen(ProfileToken) + strlen(velocity_x) + strlen(velocity_y) + 9 + 11);
-    client.println(1050);
-    client.println();
-//    Serial.println(millis());	
-    client.print(soap_EnvelopeOpen);
-//    client.print(Serialize_Header(soap_Header))
-//    client.print(soap_Header->soap_HeaderOpen);
-//    client.print(soap_Header->soap_HeaderSecurity);
-//    client.print(soap_Header->soap_HeaderClose);
-    Serial.print(Serialize_Header(soap_Header));
-//    Serial.println(millis());
-//    client.print(//Serialize_Body(soap_Body));
-    client.print("<s:Header><Security s:mustUnderstand=\"1\" xmlns=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd\"><UsernameToken><Username>admin</Username><Password Type=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordDigest\">K2IcvAJ0Mq/4Rv9jYyrKBsgiJ1c=</Password><Nonce EncodingType=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary\">TEbs4ad+2UWRiWKbPRxRyboEAAAAAA==</Nonce><Created xmlns=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd\">2022-02-11T22:32:40.593Z</Created></UsernameToken></Security></s:Header>");
-    client.print(soap_Body->soap_BodyOpen);    
-    client.print("<ContinuousMove xmlns=\"http://www.onvif.org/ver20/ptz/wsdl\"><ProfileToken>");
-    client.print(ProfileToken);
-    client.print("</ProfileToken><Velocity><PanTilt x=\"");
-    client.print(velocity_x);
-    client.print("\" y=\"");
-    client.print(velocity_y);
-    client.print("\" xmlns=\"http://www.onvif.org/ver10/schema\"/></Velocity></ContinuousMove>");
-    client.print(soap_Body->soap_BodyClose);
-    client.print(soap_EnvelopeClose);
-    Serial.println(millis());	
-    Serial.println("Done sending");
-     int len = client.available();
-       while (len == 0) len = client.available();
-       while (len > 0) {
-         byte buffer[80];
-         if (len > 80) len = 80;
-         client.read(buffer, len);
-         // if (printWebData) {
-           Serial.write(buffer, len); // show in the //Serial monitor (slows some boards)
-         // }
-         // byteCount = byteCount + len;
-         len = client.available();
-       }
-    client.stop();
-    Serial.println(millis());
-  } else {
-    // if you didn't get a connection to the server:
-    Serial.println("connection failed");
-    delay(5);
-  }
-  //Serial.println("Done move");
-  // free(soap_Body.soap_Command);
-  Serial.println(millis());
+  char postData[1050];
+  char* contentType = "application/soap+xml; charset=utf-8";
+  int port = 80;
+  // Initialize the Ethernet client library
+  // with the IP address and port of the server
+  // that you want to connect to (port 80 is default for HTTP):
+  EthernetClient eth;
+  char* soapHeader = "<s:Header><Security s:mustUnderstand=\"1\" xmlns=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd\"><UsernameToken><Username>admin</Username><Password Type=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordDigest\">K2IcvAJ0Mq/4Rv9jYyrKBsgiJ1c=</Password><Nonce EncodingType=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary\">TEbs4ad+2UWRiWKbPRxRyboEAAAAAA==</Nonce><Created xmlns=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd\">2022-02-11T22:32:40.593Z</Created></UsernameToken></Security></s:Header>";
+  sprintf(postData, "%s%s%s<ContinuousMove xmlns=\"http://www.onvif.org/ver20/ptz/wsdl\"><ProfileToken>%s</ProfileToken><Velocity><PanTilt x=\"%s\" y=\"%s\" xmlns=\"http://www.onvif.org/ver10/schema\"/></Velocity></ContinuousMove>%s%s", \
+          soap_EnvelopeOpen, soapHeader, soap_Body->soap_BodyOpen, ProfileToken, velocity_x, velocity_y, soap_Body->soap_BodyClose, soap_EnvelopeClose);  
+  HttpClient client = HttpClient(eth, server, port);
+  client.post("/onvif/ptz_service", contentType, postData);
+  // read the status code and body of the response
+  int statusCode = client.responseStatusCode();
+  String response = client.responseBody();
+  Serial.print("Status code: ");
+  Serial.println(statusCode);
+  Serial.print("Response: ");
+  Serial.println(response);
+  Serial.println("Done sending");
 }
 
 char* getISOFormattedTime(NTPClient* timeClient) {
